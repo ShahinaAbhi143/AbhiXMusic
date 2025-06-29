b3b66f7b156c883cb115f587ea16344e770e96b5
Abhi Singh <156217394+ShahinaAbhi143@users.noreply.github.com>
2025-06-29 08:46:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7250012103"))
+
+OWNER_ID = int(getenv("OWNER_ID", "7907019701"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WCGKING/AbhiXMusic",
+    "https://github.com/ShahinaAbhi143/AbhiXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
